c235e5fdd4f83ee26af99d3036e64a7be309ad13
Andrew McIndoe <amcindoe@powerhrg.com>
2021-07-12 10:07:55 -0400

    Use Effect Timer Quiz Lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react"
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [timeRemaining, setTimeRemaining] = useState(10)
 
-  // add useEffect code
+  useEffect(() =>{
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+      return
+    }
+    const Timer = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1)
+    }, 1000)
+
+    
+    return function () {
+      clearTimeout(Timer)
+    }
+  }, [timeRemaining, onAnswered])
+
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
-  const { id, prompt, answers, correctIndex } = question;
+  const { id, prompt, answers, correctIndex } = question
 
   return (
     <>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
+        const isCorrect = index === correctIndex
         return (
           <button key={answer} onClick={() => handleAnswer(isCorrect)}>
             {answer}
           </button>
-        );
+        )
       })}
       <h5>{timeRemaining} seconds remaining</h5>
     </>
-  );
+  )
 }
 
-export default Question;
+export default Question
